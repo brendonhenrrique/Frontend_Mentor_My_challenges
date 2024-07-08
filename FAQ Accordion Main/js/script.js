eab9bf02912989cf8e06b0ef2b40bfb0c51733aa
Brendon <brendon.henrrique@hotmail.com>
2024-07-08 19:22:23 -0300

    terminando as funções

--- FAQ Accordion Main/js/script.js
+++ FAQ Accordion Main/js/script.js
@@ -18,21 +18,21 @@ let paragraph2 = document.querySelector(".paragraph-2");
 let paragraph3 = document.querySelector(".paragraph-3");
 let paragraph4 = document.querySelector(".paragraph-4");
 
-// Add click event listener to each FAQ header
+// Adicionar ouvinte de evento de clique a cada cabeçalho de FAQ
 headerFAQ1.addEventListener("click", toggleContent1);
 headerFAQ2.addEventListener("click", toggleContent2);
-// headerFAQ3.addEventListener("click", toggleContent3);
-// headerFAQ4.addEventListener("click", toggleContent4);
+headerFAQ3.addEventListener("click", toggleContent3);
+headerFAQ4.addEventListener("click", toggleContent4);
 
 function toggleContent1() {
-    // Check if minus icon is displayed
+    // Verifique se o ícone de menos é exibido
     if (iconMinus1.style.display === "block") {
-      // Hide answer content and minus icon
+      // Ocultar o conteúdo da resposta e o ícone de menos
       headerFAQ1.innerHTML = `<h3>What is Frontend Mentor, and how will it help me?</h3> <img class="icon-plus-1" src="./assets/images/icon-plus.svg" alt="icon-plus"">`;
       paragraph1.innerHTML = " ";
       iconMinus1.style.display = "none";
     } else {
-      // Show answer content and minus icon
+      // Mostrar o conteúdo da resposta e o ícone de menos
       headerFAQ1.innerHTML = `<h3>What is Frontend Mentor, and how will it help me?</h3> <img class="icon-minus-1" src="./assets/images/icon-minus.svg" alt="icon-minus">`;
       paragraph1.innerHTML = `<p>
         Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. Its suitable for all levels and ideal for portfolio building.
@@ -40,7 +40,7 @@ function toggleContent1() {
       iconMinus1.style.display = "block";
     }
   
-    // Always show the plus button
+    // Sempre mostre o botão de adição
     iconPlus1.style.display = "block";
   }
 
@@ -73,9 +73,43 @@ function toggleContent2() {
 }
 
 function toggleContent3() {
-  // Similar logic for other FAQs
+  if(iconMinus3.style.display === "block") {
+    headerFAQ3.innerHTML = `<h3>Can I use Frontend Mentor projects in my portfolio?</h3> <img class="icon-plus-1" src="./assets/images/icon-plus.svg" alt="icon-plus"">`;
+    paragraph3.innerHTML = " ";
+    iconMinus3.style.display = "none"
+  } else {
+    headerFAQ3.innerHTML = `<h3>Can I use Frontend Mentor projects in my portfolio?</h3> <img class="icon-minus-1" src="./assets/images/icon-minus.svg" alt="icon-minus">`;
+    paragraph3.innerHTML = `<p>
+         Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!
+      </p>`;
+      iconMinus3.style.display = "block";
+  }
 }
 
 function toggleContent4() {
-  // Similar logic for other FAQs
+  if(iconMinus4.style.display === "block") {
+    headerFAQ4.innerHTML = `<h3>How can I get help if I'm stuck on a challenge?</h3>`;
+    iconMinus4.style.display = "none";
+    paragraph4.innerHTML= ""
+    const img4 = document.createElement("img")
+    img4.classList.add("icon-minus-4"); // Usando classList para uma abordagem mais moderna
+    img4.src = "./assets/images/icon-plus.svg"; // Usando dotNotation e o atributo para uma abordagem amis moderna que setAttribute
+    img4.alt = "icon-plus";
+    headerFAQ4.appendChild(img4)
+    iconPlus4.style.display = "block"
+  } else {
+    headerFAQ4.textContent = ""
+    const h3 = document.createElement("h3")
+    h3.innerText = "How can I get help if I'm stuck on a challenge?"
+    const img4 = document.createElement("img")
+    img4.classList.add("icon-minus-4")
+    img4.src = "./assets/images/icon-minus.svg"; // Usando dotNotation e o atributo para uma abordagem amis moderna que setAttribute
+    img4.alt = "icon-minus";
+    const p = document.createElement("p")
+    p.textContent = "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+    headerFAQ4.appendChild(h3);
+    headerFAQ4.appendChild(img4);
+    paragraph4.appendChild(p);
+    iconMinus4.style.display = "block";
+  }
 }
