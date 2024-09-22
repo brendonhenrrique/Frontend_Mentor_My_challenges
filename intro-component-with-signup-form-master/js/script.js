b621f28e3bae6db0adfb40213357832d0f507d4f
brendonhenrrique <brendon.henrrique@gmail.com>
2024-09-22 14:12:08 -0300

    projeto introdução formulário 80%

---
+++ intro-component-with-signup-form-master/js/script.js
@@ -0,0 +1,12 @@
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+const buttonFreeTrial = document.getElementById("freeTrial");
+
+buttonFreeTrial.addEventListener("click",() => {
+    if (firstName || lastName || email || password === "") {
+        
+    }
+});
